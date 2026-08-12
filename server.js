c01cbb2ebb015218d18a72a6a6ad884a03f3f8a4
ahmed-tarek <c.tarek2015@gmail.com>
2026-08-12 15:00:02 +0300

    app is built

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 
+const methodOverride = require("method-override");
+
 const app = express();
 const mongoose = require("mongoose");
 
 const morgan = require("morgan");
-app.use(morgan("dev"));
+
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
@@ -22,13 +26,13 @@ const Fruit = require("./models/fruit");
 
 // GET /
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
+  res.render("home.ejs");
 });
 
 app.get("/fruits", async (req, res) => {
   try {
     const fruits = await Fruit.find();
-    res.send(fruits);
+    res.render("fruits/index.ejs", { fruits });
   } catch (err) {
     console.log(err.message);
     res.send("failed to get all fruits");
@@ -47,6 +51,53 @@ app.post("/fruits", async (req, res) => {
       req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
+
+    res.redirect("/fruits");
+  } catch (err) {
+    console.log(err.message);
+    res.send("failed to create");
+  }
+});
+
+app.get("/fruits/:id", async (req, res) => {
+  try {
+    const fruit = await Fruit.findById(req.params.id);
+    res.render("fruits/show.ejs", { fruit });
+  } catch (err) {
+    console.log(err.message);
+    res.send("failed to get fruit");
+  }
+});
+
+app.delete("/fruits/:id", async (req, res) => {
+  try {
+    await Fruit.findByIdAndDelete(req.params.id);
+    res.redirect("/fruits");
+  } catch (err) {
+    console.log(err.message);
+    res.send("failed to delete fruit");
+  }
+});
+
+app.get("/fruits/:id/edit", async (req, res) => {
+  try {
+    const fruit = await Fruit.findById(req.params.id);
+    res.render("fruits/edit.ejs", { fruit });
+  } catch (err) {
+    console.log(err.message);
+    res.send("failed to update fruit");
+  }
+});
+
+app.put("/fruits/:id", async (req, res) => {
+  try {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect(`/fruits/${req.params.id}`);
 
     res.redirect("/fruits");
   } catch (err) {
